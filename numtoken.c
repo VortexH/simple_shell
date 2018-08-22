acc4cbf64ee19bfa0cc5405fd28269f1d3b1b419
VortexH <hemant.heer@gmail.com>
2018-08-22 22:04:28 +0000

    built a function for calculating the number of tokens in the buffer

---
+++ numtoken.c
@@ -0,0 +1,21 @@
+int numToken(char *buffer)
+{
+	char *delims;
+	int numTokes, i, j;
+
+	delims = " \n";
+	numTokes = 0;
+	for (i = 0; buffer[i]; i++)
+	{
+		for (j = 0; delims[j]; j++)
+		{
+			if (buffer[i] == delims[j])
+			{
+				numTokes++;
+				break;
+			}
+		}
+	}
+
+	return (numTokes);
+}
